920b983ac5439255f19a564844bab7d46973d3e8
DESKTOP-9Q0P235\Joel <joliva4@institutjaumehuguet.cat>
2024-10-28 15:58:39 +0100

    51 52 acabats

--- EXERCICIS 9. Tipus de dades del 51 al 60/51.py
+++ EXERCICIS 9. Tipus de dades del 51 al 60/51.py
@@ -4,9 +4,7 @@ vector = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 # Inicialitzem una variable per la suma
 contador = 0
 
-# Recorrem el vector i sumem els elements
 for num in vector:
     contador += num
 
-# Mostrem la suma total
 print(f'La suma de totes les posicions és: {contador}')
---
+++ EXERCICIS 9. Tipus de dades del 51 al 60/52.py
@@ -0,0 +1,18 @@
+# Crea un programa que vagi demanant a l'usuari introduir números per teclat fins a introduir el número 0. Cada número introduït s'afegirà a una llista. Un cop introduïts tots els números, es mostrarà per pantalla  tots ells en una única línia, així com la suma de tots ells en una altra línia i la quantitat de números introduïts en una altra. S'haurà de validar que l'usuari no introdueixi caràcters no numèrics.
+
+llista =  []
+contador = 0
+while True:
+    numeros = int(input("Introdueix un numero : "))
+    llista.append(numeros)
+    contador = sum(llista)
+
+
+    print(llista)
+    print  (contador)
+    print(f"La suma de les de els numeros introduits és: {contador}")
+    print (f"Hi han {len(llista)} numeros a la llista ")
+
+    if  numeros == 0:
+        break
+
